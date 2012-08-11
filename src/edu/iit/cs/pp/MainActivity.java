f6bd37184214b0924def8a9c2def132de57c8097
Bo Feng <bfeng1216@gmail.com>
2012-08-11 13:58:41 -0500

    more details for activity results

--- src/edu/iit/cs/pp/MainActivity.java
+++ src/edu/iit/cs/pp/MainActivity.java
@@ -1,10 +1,20 @@
 package edu.iit.cs.pp;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.londatiga.android.CropOption;
+import net.londatiga.android.CropOptionAdapter;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -12,6 +22,8 @@ import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * This is the main entry point from which the app gets started. Class name is
@@ -21,7 +33,12 @@ import android.widget.Button;
  * 
  */
 public class MainActivity extends Activity {
+	private Uri mImageCaptureUri;
+	private ImageView mImageView;
 
+	private static final int PICK_FROM_CAMERA = 1;
+	private static final int CROP_FROM_CAMERA = 2;
+	private static final int PICK_FROM_FILE = 3;
 	Button connectGoogle;
 	Button connectFacebook;
 
@@ -63,17 +80,19 @@ public class MainActivity extends Activity {
 						LocalAlbumsList.class);
 				startActivity(localAlbumsList);
 			} else {
-				Intent intent 	 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-				
-				Uri mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(),
-								   "tmp_avatar_" + String.valueOf(System.currentTimeMillis()) + ".jpg"));
+				Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-				intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+				mImageCaptureUri = Uri.fromFile(new File(Environment
+						.getExternalStorageDirectory(), "tmp_avatar_"
+						+ String.valueOf(System.currentTimeMillis()) + ".jpg"));
+
+				intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT,
+						mImageCaptureUri);
 
 				try {
 					intent.putExtra("return-data", true);
-					
-					startActivityForResult(intent, 1);
+
+					startActivityForResult(intent, PICK_FROM_CAMERA);
 				} catch (ActivityNotFoundException e) {
 					e.printStackTrace();
 				}
@@ -81,5 +100,127 @@ public class MainActivity extends Activity {
 
 		}
 
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode != RESULT_OK)
+			return;
+
+		switch (requestCode) {
+		case PICK_FROM_CAMERA:
+			doCrop();
+
+			break;
+
+		case PICK_FROM_FILE:
+			mImageCaptureUri = data.getData();
+
+			doCrop();
+
+			break;
+
+		case CROP_FROM_CAMERA:
+			Bundle extras = data.getExtras();
+
+			if (extras != null) {
+				Bitmap photo = extras.getParcelable("data");
+
+				mImageView.setImageBitmap(photo);
+			}
+
+			File f = new File(mImageCaptureUri.getPath());
+
+			if (f.exists())
+				f.delete();
+
+			break;
+
+		}
+	}
+
+	private void doCrop() {
+		final ArrayList<CropOption> cropOptions = new ArrayList<CropOption>();
+
+		Intent intent = new Intent("com.android.camera.action.CROP");
+		intent.setType("image/*");
+
+		List<ResolveInfo> list = getPackageManager().queryIntentActivities(
+				intent, 0);
+
+		int size = list.size();
+
+		if (size == 0) {
+			Toast.makeText(this, "Can not find image crop app",
+					Toast.LENGTH_SHORT).show();
+
+			return;
+		} else {
+			intent.setData(mImageCaptureUri);
+
+			intent.putExtra("outputX", 200);
+			intent.putExtra("outputY", 200);
+			intent.putExtra("aspectX", 1);
+			intent.putExtra("aspectY", 1);
+			intent.putExtra("scale", true);
+			intent.putExtra("return-data", true);
+
+			if (size == 1) {
+				Intent i = new Intent(intent);
+				ResolveInfo res = list.get(0);
+
+				i.setComponent(new ComponentName(res.activityInfo.packageName,
+						res.activityInfo.name));
+
+				startActivityForResult(i, CROP_FROM_CAMERA);
+			} else {
+				for (ResolveInfo res : list) {
+					final CropOption co = new CropOption();
+
+					co.title = getPackageManager().getApplicationLabel(
+							res.activityInfo.applicationInfo);
+					co.icon = getPackageManager().getApplicationIcon(
+							res.activityInfo.applicationInfo);
+					co.appIntent = new Intent(intent);
+
+					co.appIntent
+							.setComponent(new ComponentName(
+									res.activityInfo.packageName,
+									res.activityInfo.name));
+
+					cropOptions.add(co);
+				}
+
+				CropOptionAdapter adapter = new CropOptionAdapter(
+						getApplicationContext(), cropOptions);
+
+				AlertDialog.Builder builder = new AlertDialog.Builder(this);
+				builder.setTitle("Choose Crop App");
+				builder.setAdapter(adapter,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int item) {
+								startActivityForResult(
+										cropOptions.get(item).appIntent,
+										CROP_FROM_CAMERA);
+							}
+						});
+
+				builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+					@Override
+					public void onCancel(DialogInterface dialog) {
+
+						if (mImageCaptureUri != null) {
+							getContentResolver().delete(mImageCaptureUri, null,
+									null);
+							mImageCaptureUri = null;
+						}
+					}
+				});
+
+				AlertDialog alert = builder.create();
+
+				alert.show();
+			}
+		}
 	}
 }
